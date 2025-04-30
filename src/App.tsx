31e2bb82ec91f344ce3474b1165e976af7b2b585
Mateoskix <mateogt0903@gmail.com>
2025-04-30 14:05:23 -0500

    feat: add dummy colors for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold text-green-500'>Hello World!</h1>
+      <h1 className='text-3xl font-bold text-primary'>Hello World!</h1>
     </>
   )
 }
