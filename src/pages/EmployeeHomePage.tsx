9879c755a064ca0095f205cb6657e07c08de260d
Mateoskix <mateogt0903@gmail.com>
2025-05-05 19:35:04 -0500

    feat: created page, added global font

--- src/pages/EmployeeHomePage.tsx
+++ src/pages/EmployeeHomePage.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const EmployeeHomePage = () => {
   return (
-    <div>EmployeeHomePage</div>
+    <section className="bg-background p-56 flex flex-col items-center h-screen">
+      <h1 className='text-primary font-semibold text-[56px]'>Employee Portal</h1>
+      <div></div>
+    </section>
   )
 }
 
