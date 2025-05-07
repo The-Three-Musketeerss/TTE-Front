8b0934ac8dc1ed01b672d2fdb3ae44e432249d71
Mateoskix <mateogt0903@gmail.com>
2025-05-07 10:55:04 -0500

    chore: removed unused imports

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,5 +1,5 @@
 import Button from "@components/shared/Button/Button";
-import React, { use, useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import CreateCategory from "@components/Homepage/CreateCategory/CreateCategory";
 
