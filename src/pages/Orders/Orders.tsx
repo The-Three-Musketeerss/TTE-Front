19d494e8a9585a6864663c603862a019cba3eda4
Miguel Jaramillo Arenas <migueljaramillo879@gmail.com>
2025-05-07 10:02:36 -0500

    Refactor padding

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -33,7 +33,7 @@ const OrdersPage = () => {
   ];
 
   return (
-    <div className="bg-gray-100 min-h-screen px-40">
+    <div className="bg-gray-100 min-h-screen px-10 lg:px-40">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-semibold">My Orders</h1>
       </div>
