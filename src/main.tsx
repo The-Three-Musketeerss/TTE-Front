2ef6c5e1fd6485c81b23f8f71c0bac90cd3d16e8
Miguel Jaramillo Arenas <migueljaramillo879@gmail.com>
2025-05-07 01:48:13 -0500

    Add layout to router

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 //Pages
 import EmployeeHomePage from '@pages/Homepage/Homepage';
+import MainLayout from "@layouts/MainLayout";
+import AdminLayout from "@layouts/AdminLayout";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<EmployeeHomePage/>} />
+      <Route element={<AdminLayout />}>
+        <Route path="/" element={<EmployeeHomePage/>} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
