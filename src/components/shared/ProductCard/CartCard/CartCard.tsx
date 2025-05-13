d541852fad90672cd637dd82ec134fa05f40d48d
Mateoskix <mateogt0903@gmail.com>
2025-05-12 22:42:19 -0500

    Created order summary and shipping info components

--- src/components/shared/ProductCard/CartCard/CartCard.tsx
+++ src/components/shared/ProductCard/CartCard/CartCard.tsx
@@ -1,21 +1,36 @@
 import { deleteFromCart, getCart } from "@services/CartServices";
+import { getProductById } from "@services/ProductServices";
 import { ProductProps } from "@utils/types";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
-type Props = ProductProps & {
+type Props = {
   token: string;
+  id: number;
+  quantity: number;
   setCart: any;
 };
 
 const CartCard = ({
   id,
-  image,
-  price,
-  title,
-  quantity,
   token,
   setCart,
+  quantity,
 }: Props) => {
+  const [product, setProduct] = useState<ProductProps>();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await getProductById(id.toString());
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Failed to fetch product:", error);
+      }
+    };
+    fetchProduct();
+  }, [id, token]);
+
   const handleRemoveFromCart = () => {
     toast.promise(deleteFromCart(token, id!), {
       loading: "Removing from cart...",
@@ -31,12 +46,16 @@ const CartCard = ({
     });
   };
 
+  if (!product) {
+    return <div></div>;
+  }
+
   return (
-    <div className="bg-gray-100 flex items-start p-4 rounded-md shadow-sm w-full max-w-md">
+    <div className="bg-white flex items-start p-4 rounded-md shadow-sm w-full">
       <div className="w-20 h-20 bg-gray-300 rounded-md mr-4 flex-shrink-0">
         <img
-          src={image}
-          alt={title}
+          src={product.image}
+          alt={product.title}
           className="w-full h-full object-cover rounded-md"
         />
       </div>
@@ -44,9 +63,9 @@ const CartCard = ({
       <div className="flex flex-col justify-between w-full">
         <div className="flex justify-between items-start">
           <div>
-            <h3 className="text-sm font-semibold">{title}</h3>
+            <h3 className="text-xs lg:text-sm font-semibold">{product.title}</h3>
             <p className="text-xs text-gray-600">Quantity: {quantity}</p>
-            <p className="text-base font-semibold mt-1">${price}</p>
+            <p className="text-base font-semibold mt-1">${(product.price * quantity).toFixed(2)}</p>
           </div>
         </div>
 
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -4,6 +4,8 @@ import { CartItemProps } from "@utils/types";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CartCard from "@components/shared/ProductCard/CartCard/CartCard";
+import Shipping from "@components/Cart/Shipping/Shipping";
+import OrderSummary from "@components/Cart/OrderSummary/OrderSummary";
 
 const Cart = () => {
   const [cart, setCart] = useState<CartItemProps>();
@@ -37,23 +39,30 @@ const Cart = () => {
     );
   } else {
     return (
-      <section>
-        <h1 className="font-semibold text-4xl">Your Cart</h1>
-        <Link to={"/listing"} className="font-medium">
+      <section className="flex flex-col lg:flex-row items-center justify-center 2xl:justify-start lg:items-start gap-4 2xl:gap-8 p-4 2xl:p-8">
+        <div className="max-w-md 2xl:max-w-4xl">
+        <h1 className="font-semibold text-2xl lg:text-4xl">Your Cart</h1>
+        <Link to={"/listing"} className="font-medium text-sm mt-2 lg:text-base">
           Not ready to checkout? Continue Shopping
         </Link>
-        {cart?.shoppingCart.map((item) => (
-          <CartCard
-            title="test"
-            price={99}
-            image="https://via.placeholder.com/150"
-            key={item.productId}
-            id={item.productId}
-            quantity={item.quantity}
-            token={user?.token}
-            setCart={setCart}
-          />
-        ))}
+        <div className="flex flex-col gap-4 mt-4 overflow-y-scroll h-80">
+          {cart?.shoppingCart.map((item) => (
+            <CartCard
+              key={item.productId}
+              id={item.productId}
+              quantity={item.quantity}
+              token={user?.token}
+              setCart={setCart}
+            />
+          ))}
+        </div>
+        <Shipping />
+        </div>
+        <OrderSummary
+          subtotal={cart?.totalAfterDiscount!}
+          shipping={cart?.shippingCost!}
+          total={cart?.finalTotal!}
+        />
       </section>
     );
   }
