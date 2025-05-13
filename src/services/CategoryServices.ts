f851f7e66a25ca25d9319bad67279ffc84c85460
Mateoskix <mateogt0903@gmail.com>
2025-05-13 14:33:05 -0500

    added create category service

--- src/services/CategoryServices.ts
+++ src/services/CategoryServices.ts
@@ -1,15 +1,33 @@
 const rootUrl = import.meta.env.VITE_API_URL;
 export const getCategories = async () => {
-    const response = await fetch(`${rootUrl}/api/categories`);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(`${rootUrl}/api/categories`);
+  const data = await response.json();
+  return data;
+};
 
 export const getTopCategories = async () => {
-    const response = await fetch(`${rootUrl}/api/categories/top`);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(`${rootUrl}/api/categories/top`);
+  const data = await response.json();
+  return data;
+};
+
+export const createCategory = async (name: string, token: string) => {
+  const response = await fetch(`${rootUrl}/api/categories`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name }),
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to create category");
+  }
+
+  return data;
+};
 
 export const updateCategory = async (
   id: string,
