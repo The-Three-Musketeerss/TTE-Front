659448d6053d1058856a359d76103f41c0fc7153
Miguel Jaramillo Arenas <migueljaramillo879@gmail.com>
2025-05-18 20:30:52 -0500

    Add tests setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: 'coverage',
+      reporter: ['text', 'html', 'lcov'],
+      exclude: ['**/test/**', '**/*.test.tsx', '**/*.test.ts'],
+    }
+  }
 })
