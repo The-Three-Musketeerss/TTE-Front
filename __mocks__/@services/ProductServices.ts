b5b309712050bb7a10f1fb4cbfb961003ad286a9
Miguel Jaramillo Arenas <migueljaramillo879@gmail.com>
2025-06-04 16:59:37 -0500

    Add order stories

--- __mocks__/@services/ProductServices.ts
+++ __mocks__/@services/ProductServices.ts
@@ -6,7 +6,7 @@ export const getProductById = async (id: string) => {
       price: 49.99,
       description: "This is a mocked product description.",
       category: "Electronics",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1558769132-cb1aea458c5e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       inventory: {
         total: 100,
         available: 80,
